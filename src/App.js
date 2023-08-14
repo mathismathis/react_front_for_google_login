c87e8b5c6366d60bdfed73b95ae43e40874ad039
Prashant Singh <psingh5025615@gmail.com>
2023-08-14 14:52:14 +0530

    issue solved

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import LoginPage from './components/LoginPage';
 import MainPage from './components/MainPage';
 import axios from 'axios';
+import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 
 
 function App() {
@@ -9,27 +11,60 @@ function App() {
 
   const handleLogin = (userData) => {
     setUser(userData);
+    localStorage.setItem('user', JSON.stringify(userData));
   };
+  
+  
 
   const handleLogout = async () => {
     try {
-      await axios.get('http://127.0.0.1:8000/api/logout/');  
-      setUser(null); 
+      console.log("start")
+      await axios.get('http://127.0.0.1:8000/api/logout/');
+      setUser(null);
+      console.log("end")
     } catch (error) {
       console.error('Error logging out:', error);
     }
   };
 
+  useEffect(() => {
+    const userStr = localStorage.getItem('user');
+    if (userStr) {
+      const user = JSON.parse(userStr);
+      setUser(user);
+    }
+  }, [])
+
+  useEffect(() => {
+    return () => {
+      // this is the cleanup ... function 
+    }
+  }, [user])
+
+
+
   return (
+    <BrowserRouter>
     <div className="App">
       <header className="App-header">
-        {user ? (
+        {/* {user ? (
           <MainPage userData={user} onLogout={handleLogout} />
         ) : (
           <LoginPage onLogin={handleLogin} />
-        )}
+        )} */}
+
+        {
+          user !== null ? (
+            <MainPage userData={user} onLogout={handleLogout} />
+          ) : (
+            <LoginPage onLogin={handleLogin} />
+          )
+        }
+
+
       </header>
     </div>
+    </BrowserRouter>
   );
 }
 
